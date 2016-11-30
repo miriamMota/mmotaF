ffb38a61766df618654a23f3229508a59b0ded16
Miriam Mota <mimifoixbcn@gmail.com>
2016-11-30 08:54:13 +0100

    Afegim funcio descPlots

---
+++ R/descPlot.R
@@ -0,0 +1,27 @@
+#' A descPlot Function
+#'
+#' Genera graficos univariantes para todas las variables que se indiquen, el formato de entrada es "data.frame"
+#' @param dat data frame que contiene las variables a graficar. 
+#' @param topdf valor lógico indicando si se quieren guardar los gráficos en pdf. Por defecto FALSE.
+#' @param nameFile nombre del fichero (tipo caracter) donde guardar los gráficos. Por defecto "descriptive_plots.pdf". 
+#' @export descPlot
+#' @author Miriam Mota \email{mmota.foix@@gmail.com}
+#' @examples
+#' df <- data,frame(y = as.factor(c(0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1)),X = c(20,2,1,2,23,3,4,5,15,-5,2,1,3,10,4,87,5,3,7,0,1,-2))
+#' descPlot(df)
+#' @keywords plots descriptive 
+
+
+descPlot <- function(data, topdf = FALSE, nameFile = "descriptive_plots.pdf"){
+  if (topdf) pdf(nameFile)
+  par(mfrow = c(3,2))
+  for (i in 2:dim(data)[2]) {
+    if (class(data[,i]) == "factor") {plot(data[,i], xlab = names(data)[i], main = "Histograma")
+    }else {
+      hist(data[,i], xlab = names(data)[i], main = "Histograma")
+    }
+  }
+  if (topdf) dev.off()
+}
+
+
