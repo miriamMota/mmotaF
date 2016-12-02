686fa261d9a2cb623004c78b027de546a7730628
Miriam Mota <mimifoixbcn@gmail.com>
2016-12-02 10:48:49 +0100

    millores descPlot

--- R/descPlot.R
+++ R/descPlot.R
@@ -4,6 +4,9 @@
 #' @param dat data frame que contiene las variables a graficar. 
 #' @param topdf valor lógico indicando si se quieren guardar los gráficos en pdf. Por defecto FALSE.
 #' @param nameFile nombre del fichero (tipo caracter) donde guardar los gráficos. Por defecto "descriptive_plots.pdf". 
+#' @param color nombre del color para pintar el gráfico
+#' @param nrow.par número de filas que mostrar en la interficie gráfica 
+#' @param ncol.par número de columnas que mostrar en la interficie gráfica 
 #' @export descPlot
 #' @author Miriam Mota \email{mmota.foix@@gmail.com}
 #' @examples
@@ -12,17 +15,26 @@
 #' @keywords plots descriptive 
 
 
-descPlot <- function(dat, topdf = FALSE, nameFile = "descriptive_plots.pdf"){
-  if (topdf) pdf(nameFile)
-  par(mfrow = c(4,2))
+descPlot <- function(dat, topdf = FALSE, nameFile = "descriptive_plots.pdf", color = "gray48", nrow.par = 4, ncol.par = 2) 
+{
+  if (topdf) 
+    pdf(nameFile)
+  par(mfrow = c(nrow.par, ncol.par))
   for (i in 2:dim(dat)[2]) {
-    if (class(dat[,i]) == "factor") {
-      try(plot(dat[,i], xlab = names(dat)[i], main = "Diagrama de barras"),TRUE)
-    }else {
-      try(hist(dat[,i], xlab = names(dat)[i], main = "Histograma"),TRUE)
+    if (class(dat[, i]) == "factor") {
+      try(plot(dat[, i], xlab = names(dat)[i], main = "Diagrama de barras", col = makeTransparent(color)), 
+          TRUE)
+    }
+    else {
+      # par(mar = c(3.1, 3.1, 1.1, 2.1))
+      try(hist(dat[, i], xlab = names(dat)[i], main = "Histograma", col = makeTransparent(color)), TRUE)
+      try(rug(dat[,i]))
+      # try(boxplot(dat[,i], horizontal = TRUE, outline = TRUE, frame = FALSE,  col = "green1", add = TRUE),TRUE)
     }
   }
-  if (topdf) dev.off()
+  if (topdf) 
+    dev.off()
 }
+
 
 
