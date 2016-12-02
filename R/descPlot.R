8bbacffd3db77ccbce8eb91eec9955c354036a4e
Miriam Mota <mimifoixbcn@gmail.com>
2016-12-02 09:26:14 +0100

    correcció errors descPlot

--- R/descPlot.R
+++ R/descPlot.R
@@ -7,12 +7,12 @@
 #' @export descPlot
 #' @author Miriam Mota \email{mmota.foix@@gmail.com}
 #' @examples
-#' df <- data,frame(y = as.factor(c(0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1)),X = c(20,2,1,2,23,3,4,5,15,-5,2,1,3,10,4,87,5,3,7,0,1,-2))
-#' descPlot(data = df)
+#' df <- data.frame(y = as.factor(c(0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1)),X = c(20,2,1,2,23,3,4,5,15,-5,2,1,3,10,4,87,5,3,7,0,1,-2))
+#' descPlot(dat = df)
 #' @keywords plots descriptive 
 
 
-descPlot <- function(data, topdf = FALSE, nameFile = "descriptive_plots.pdf"){
+descPlot <- function(dat, topdf = FALSE, nameFile = "descriptive_plots.pdf"){
   if (topdf) pdf(nameFile)
   par(mfrow = c(4,2))
   for (i in 2:dim(dat)[2]) {
