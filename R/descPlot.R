4d7c82a41a35d0f423900e99b0bcd2a5105d5032
Miriam Mota <mimifoixbcn@gmail.com>
2016-12-02 11:07:53 +0100

    correcció errors descPlot

--- R/descPlot.R
+++ R/descPlot.R
@@ -10,17 +10,16 @@
 #' @export descPlot
 #' @author Miriam Mota \email{mmota.foix@@gmail.com}
 #' @examples
-#' df <- data.frame(y = as.factor(c(0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1)),X = c(20,2,1,2,23,3,4,5,15,-5,2,1,3,10,4,87,5,3,7,0,1,-2))
-#' descPlot(dat = df)
+#' df <- data.frame(Y = as.factor(c(0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1)),X = c(20,2,1,2,23,3,4,5,15,-5,2,1,3,10,4,87,5,3,7,0,1,-2))
+#' descPlot(dat = df, color = "red", nrow.par = 1, ncol.par = 2)
 #' @keywords plots descriptive 
 
 
 descPlot <- function(dat, topdf = FALSE, nameFile = "descriptive_plots.pdf", color = "gray48", nrow.par = 4, ncol.par = 2) 
 {
-  if (topdf) 
-    pdf(nameFile)
+  if (topdf)   pdf(nameFile)
   par(mfrow = c(nrow.par, ncol.par))
-  for (i in 2:dim(dat)[2]) {
+  for (i in 1:dim(dat)[2]) {
     if (class(dat[, i]) == "factor") {
       try(plot(dat[, i], xlab = names(dat)[i], main = "Diagrama de barras", col = makeTransparent(color)), 
           TRUE)
@@ -32,8 +31,7 @@ descPlot <- function(dat, topdf = FALSE, nameFile = "descriptive_plots.pdf", col
       # try(boxplot(dat[,i], horizontal = TRUE, outline = TRUE, frame = FALSE,  col = "green1", add = TRUE),TRUE)
     }
   }
-  if (topdf) 
-    dev.off()
+  if (topdf)    dev.off()
 }
 
 
