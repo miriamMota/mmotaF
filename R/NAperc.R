f082ac1e5244aa387b3466472fa1d30e97a4f206
Miriam Mota <mimifoixbcn@gmail.com>
2017-03-22 15:14:59 +0100

    canvi nom funcio NAvar

--- R/NAperc.R
+++ R/NAperc.R
@@ -1,13 +1,13 @@
-#' A NAvar Function
+#' A NAperc Function
 #'
 #' Percentatge de dades faltants per variable (na$perc) i variables amb més de "x" percentatge(na$var). 
 #' @param dat data frame que contiene las variables a evaluar 
 #' @param maxNA Porcentafe de NA permitido 
-#' @export NAvar
+#' @export NAperc
 #' @author Miriam Mota \email{mmota.foix@@gmail.com}
 #' @keywords na missing percentage 
 
-NAvar <- function(df, maxNA = 80){
+NAperc <- function(df, maxNA = 80){
 na <- list()
 na$perc <- sort( apply(df,2,function(x) round((sum(is.na(x))/length(x) )*100,2)) )
 na$var <- names(na$perc )[na$perc  > maxNA]
