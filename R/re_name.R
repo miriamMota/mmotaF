3db3d037cdb9694e26cda5a00ce2acc4e333654b
miriam <mimifoixbcn@gmail.com>
2017-05-19 14:45:15 +0200

    re_name

---
+++ R/re_name.R
@@ -0,0 +1,18 @@
+
+#' A re_name Function
+#'
+#' Renombra los identificadores de columna, substituyendo "." por "_", eliminando acentos, ec.
+#' @param dat data frame que contiene las variables a evaluar 
+#' @export re_name
+#' @author Miriam Mota \email{mmota.foix@@gmail.com}
+#' @keywords names dataframe correct
+
+re_name <- function(data){
+  names(data) <- gsub(".", "_",gsub("..", ".",gsub("..", ".", names(data),fixed = T), fixed = T),fixed = T)
+  names(data) <- gsub("_$","",names(data))
+  names(data) <- gsub("^X_","",names(data))
+  names(data) <- chartr("áéóíú", "aeoiu", names(data))
+  return(data)
+}
+
+
