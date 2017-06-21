95bdd8e8ccb6a2073495f7244147ccebbe2ea8a4
miriammota <miriam.mota@vhir.org>
2017-06-21 15:50:24 +0200

    re_name

--- R/re_name.R
+++ R/re_name.R
@@ -8,7 +8,7 @@
 #' @keywords names dataframe correct
 
 re_name <- function(data){
-  names(data) <- gsub(".", "_",gsub("..", ".",gsub("..", ".", names(data),fixed = T), fixed = T),fixed = T)
+  names(data) <- gsub(".", "_",gsub("..", ".", gsub("..", ".",gsub("..", ".", names(data),fixed = T), fixed = T),fixed = T),fixed = T)
   names(data) <- gsub("_$","",names(data))
   names(data) <- gsub("^X_","",names(data))
   names(data) <- chartr("áéóíúÁÉÍÓÚ", "aeoiuAEIOU", names(data))
