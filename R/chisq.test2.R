1119e3533e804d34b774d1f4d69f815524e12bb2
miriammota <mmota.foix@gmail.com>
2017-12-04 10:15:22 +0100

    chisq.test2

---
+++ R/chisq.test2.R
@@ -0,0 +1,22 @@
+chisq.test2 <-
+  function(obj, chisq.test.perm){
+    if (any(dim(obj)<2) || is.null(dim(obj)) || sum(rowSums(obj)>0)<2 || sum(colSums(obj)>0)<2)
+      return(NaN)
+    obj<-obj[,colSums(obj)>0] # erase columns full of zeros.
+    expect<-outer(rowSums(obj),colSums(obj))/sum(obj)    
+    if (any(expect<5)){
+      if (chisq.test.perm)
+        test<-try(chisq.test(obj, simulate.p.value=TRUE, correct = F),silent=TRUE)          
+      else 
+        test <- try(fisher.test(obj),silent=TRUE)
+    } else {
+      test <- try(chisq.test(obj, correct = F),silent=TRUE)
+    }
+    if (inherits(test,"try-error")){
+      test<-try(chisq.test(obj, simulate.p.value=TRUE, correct = F),silent=TRUE)  
+    }
+    if (inherits(test,"try-error"))
+      return(NaN)
+    ans <- test$p.value
+    ans
+  }
