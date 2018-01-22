5a755c5a83818cda7d6133bd04e08cec458b2daf
miriammota <mmota.foix@gmail.com>
2018-01-22 08:39:57 +0100

    canvi a la funció re_name

--- R/chisq.test2.R
+++ R/chisq.test2.R
@@ -4,8 +4,7 @@
 #' @export chisq.test2
 #' @keywords chisquare test comparegroups yates correction
 
-chisq.test2 <-
-  function(obj, chisq.test.perm){
+chisq.test2 <- function(obj, chisq.test.perm, correct = FALSE ){
     if (any(dim(obj) < 2) || is.null(dim(obj)) || sum(rowSums(obj) > 0) < 2 ||
         sum(colSums(obj) > 0) < 2)
       return(NaN)
@@ -13,14 +12,14 @@ chisq.test2 <-
     expect <- outer(rowSums(obj),colSums(obj))/sum(obj)
     if (any(expect < 5)) {
       if (chisq.test.perm)
-        test <- try(chisq.test(obj, simulate.p.value = TRUE, correct = F), silent = TRUE)
+        test <- try(chisq.test(obj, simulate.p.value = TRUE, correct = correct), silent = TRUE)
       else
         test <- try(fisher.test(obj),silent = TRUE)
     } else {
-      test <- try(chisq.test(obj, correct = F),silent = TRUE)
+      test <- try(chisq.test(obj, correct = correct),silent = TRUE)
     }
     if (inherits(test,"try-error")) {
-      test <- try(chisq.test(obj, simulate.p.value = TRUE, correct = F),silent = TRUE)
+      test <- try(chisq.test(obj, simulate.p.value = TRUE, correct = correct),silent = TRUE)
     }
     if (inherits(test,"try-error"))
       return(NaN)
--- R/re_name.R
+++ R/re_name.R
@@ -3,7 +3,8 @@
 #'
 #' Renombra los identificadores de columna, substituyendo '.' por '_', eliminando acentos, ec.
 #' @param dat data frame que contiene las variables a evaluar
-#' @param char simbols o caracters que es vulguin canviar per "_"
+#' @param char a character string specifying the characters to be translated. If a character vector of length 2 or more is supplied, the first element is used with a warning.
+#' @param newchar a character string specifying the translations. If a character vector of length 2 or more is supplied, the first element is used with a warning.
 #' @export re_name
 #' @author Miriam Mota \email{mmota.foix@@gmail.com}
 #' @examples
@@ -11,10 +12,10 @@
 #' 'Y/X'=as.factor(rbinom(50,1,.40)),'ibm 2' = rnorm(50,10,1))
 #' names(df) <- c('nombre+a-apellido',  'dni...NIF', 'Y/X', 'ibm 2') ## modifiquem noms per a que siguin "incorrectes"
 #' df <- re_name(data = df)
-#' df <- re_name(data = df, char = "+-")
+#' df <- re_name(data = df, char = "+-", newchar = "__")
 #' @keywords names dataframe correct
 
-re_name <- function(data,char = NULL) {
+re_name <- function(data,char = NULL, newchar = NULL) {
     names(data) <- gsub(".", "_",
                         gsub("..", ".",
                              gsub("..", ".",
@@ -27,6 +28,6 @@ re_name <- function(data,char = NULL) {
     names(data) <- gsub("^X_", "", names(data))
     names(data) <- chartr("áéóíúÁÉÍÓÚ", "aeoiuAEIOU", names(data))
     if(!is.null(char))
-    names(data) <-  chartr(char, paste(rep("_",nchar(char)), collapse = ""), names(data))
+    names(data) <-  chartr(char, newchar, collapse = ""), names(data))
     return(data)
 }
