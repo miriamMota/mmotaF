0841a096c473c8016f7260a94ed8ee1ee37e5fbb
miriammota <mmota.foix@gmail.com>
2018-05-25 08:52:39 +0200

    Ajuda data

---
+++ R/mtc_bis.R
@@ -0,0 +1,12 @@
+#' mtc_bis
+#'
+#' mtcars with specified class.
+#'
+#' @docType data
+#'
+#' @usage data(mtc_bis)
+#'
+#' @format An object of class \code{"data.frame"}; see \code{\link[qtl]{data.frame}}.
+#'
+#' @keywords datasets
+#'
