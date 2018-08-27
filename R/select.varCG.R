a9b4c450309607bae85cfced03918d95a5d53c61
miriammota <mmota.foix@gmail.com>
2018-08-27 09:51:05 +0200

    add select.varCG

---
+++ R/select.varCG.R
@@ -0,0 +1,22 @@
+#' A select.varCG Function
+#'
+#' Selección de variables según pvalor para objeto clase comparegroups.
+#' @param restab an object of class 'compareGroups'
+#' @param p.value valor de selección para el p.valor. Por defecto 0.05.
+#' @keywords compareGroups selection pvalue
+#' @export select.varCG
+#' @examples
+#' \dontrun{
+#' restab <- createTable(compareGroups(vs~., data = mtc_bis))
+#' select.varCG(restab)
+#' }
+
+select.varCG <- function(restab, p.value = 0.05){
+  pval <- as.numeric(gsub("<","",restab$descr[,"p.overall"]))
+  names(pval) <- names(restab$descr[,"p.overall"])
+  varSel <- names(which(pval < p.value))
+  nameVarSel <- NULL
+  for (i in 1:length(varSel)) nameVarSel[i] <- unlist(strsplit(varSel[i], ":"))[1]
+  return(nameVarSel)
+}
+
