29f5dceb309738af8984127f8f34f014ec42670d
miriammota <mmota.foix@gmail.com>
2018-08-30 12:22:02 +0200

    error ortografia codi

--- R/barplot_ueb.R
+++ R/barplot_ueb.R
@@ -99,7 +99,7 @@ barplot_ueb <- function(y, group = NULL, dat,
            bg = "white",
            fill = col.lev,
            cex = cex.lg, yjust = 0.5,
-           title = if.else(title.lg,ifelse(Hmisc::label(dat[,y]) == "", "", Hmisc::label(dat[,y]))), "")  ## ajustar llegenda y
+           title = ifelse(title.lg,ifelse(Hmisc::label(dat[,y]) == "", "", Hmisc::label(dat[,y]))), "")  ## ajustar llegenda y
     if (do.test) {
       if (any(table(dat[, y], dat[, group]) < 5)) {
         fishpval <- fisher.test(dat[, y], dat[, group])$p.val
