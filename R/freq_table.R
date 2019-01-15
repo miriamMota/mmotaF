b9bf5e6c343e19ae00df710c90f5af72581c4b28
miriammota <mmota.foix@gmail.com>
2019-01-15 12:00:14 +0100

    html table

--- R/freq_table.R
+++ R/freq_table.R
@@ -7,8 +7,8 @@
 #' @param margin index, or vector of indices to generate margin for
 #' @param nround integer indicating the number of decimal places (round).
 #' @param latex TRUE o FALSE, para obtener tabla en formato .tex
-#' @param sz.tab A character vector that is inserted just before the tabular environment starts. This can be used to set the font size and a variety of other table settings. Initial backslashes are automatically prefixed, if not supplied by user. Default value is 'footnotesize'
 #' @param title titles for the table
+#' @param xtab.type A character string. Possible values are latex, html, markdown, pandoc, and rst; this will be automatically determined if the function is called within knitr; it can also be set in the global option knitr.table.format. If format is a function, it must return a character string.
 #' @export freq_table
 #' @import stargazer
 #' @author Miriam Mota \email{mmota.foix@@gmail.com}
@@ -20,28 +20,27 @@ freq_table <- function(var.cat, y,
                        data, margin = NULL,
                        nround = 2,
                        latex = TRUE,
-                       sz.tab = "footnotesize",
-                       title = "titles for the table") {
+                       title = "titles for the table",
+                       xtab.type = "latex") {
 
-    data[, y] <- factor(data[, y])
-    res_freq_all <- NULL
-    for (i in 1:length(var.cat)) {
-        data[, var.cat[i]] <- factor(data[, var.cat[i]])
-        (freq_abs <- table(data[, var.cat[i]], data[, y]))
-        (freq_rel <- prop.table(freq_abs, margin = margin) * 100)
-        res_freq <- matrix(paste0(freq_abs, " (", round(freq_rel, nround), "%)"),
-                           ncol = ncol(freq_abs))
-        res_freq <- cbind(c(var.cat[i], rep("", nrow(res_freq) - 1)),
-                          rownames(freq_abs),
-                          res_freq)
-        colnames(res_freq) <- c("Variable", "Levels", colnames(freq_abs))
-        res_freq_all <- rbind(res_freq_all, rep("", ncol(res_freq)), res_freq)
-    }
-    if (latex) {
-        stargazer(res_freq_all,
-                  summary = F, type = "latex", add.lines = c(1, 2, 3),
-                  font.size = sz.tab, title = title)
-    } else {
-        return(res_freq_all)
-    }
+  data[, y] <- factor(data[, y])
+  res_freq_all <- NULL
+  for (i in 1:length(var.cat)) {
+    data[, var.cat[i]] <- factor(data[, var.cat[i]])
+    (freq_abs <- table(data[, var.cat[i]], data[, y]))
+    (freq_rel <- prop.table(freq_abs, margin = margin) * 100)
+    res_freq <- matrix(paste0(freq_abs, " (", round(freq_rel, nround), "%)"),
+                       ncol = ncol(freq_abs))
+    res_freq <- cbind(c(var.cat[i], rep("", nrow(res_freq) - 1)),
+                      rownames(freq_abs),
+                      res_freq)
+    colnames(res_freq) <- c("Variable", "Levels", colnames(freq_abs))
+    res_freq_all <- rbind(res_freq_all, rep("", ncol(res_freq)), res_freq)
+  }
+  if (latex) {
+    kable(res_freq_all, format = xtab.type, booktabs = T,
+          caption = title , longtable = TRUE, escape = F)
+  } else {
+    return(res_freq_all)
+  }
 }
